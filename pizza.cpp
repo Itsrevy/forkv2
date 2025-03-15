5462da43b02fb88ec798ae71df8b9bdbc94c168f
Itsrevy <dadaya.185@mail.ru>
2025-03-15 16:26:47 +1000

    Update pizza.cpp

--- pizza.cpp
+++ pizza.cpp
@@ -1,11 +1,3 @@
-//*****************************************************************************************************
-//      Pizza Palace Menu Order System
-//
-//      This program is a simple pizza ordering program that will calculate the total cost of the
-//      order.
-//
-//*****************************************************************************************************
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
@@ -18,9 +10,7 @@ int main() {
                  CHICKEN_NUGGETS = 7.00,
                  BREADSTICKS = 9.75;
     double numItems = 0,
-           menuItem,
            total = 0;
-    char itemLetter;
 
     cout << "=====================================================\n"
          << "\t\tWelcome to Pizza Palace\n"
@@ -28,136 +18,43 @@ int main() {
 
     cout << fixed << setprecision(2) << right;
 
-    do {
-        cout << "-----------------------------------------------------\n\n"
-             << "\tA\tPizza" << setw(26) << PIZZA
-             << "\n\tB\tSoda" << setw(27) << SODA
-             << "\n\tC\tChicken Nuggets" << setw(16) << CHICKEN_NUGGETS
-             << "\n\tD\tBreadsticks" << setw(20) << BREADSTICKS
-             << "\n\tE\tExit" << endl;
+    // Автоматически выбираем по одному блюду из каждой позиции
+    char items[] = {'A', 'B', 'C', 'D'}; // Массив с кодами блюд
+    double menuItem;
 
-        cout << "\nPlease enter the next menu item Letter: ";
-        cin >> itemLetter;
-
+    for (char itemLetter : items) {
         switch (itemLetter) {
             case 'A':
                 menuItem = PIZZA;
+                cout << "\nВыбрано: Pizza" << setw(26) << PIZZA << endl;
                 break;
             case 'B':
                 menuItem = SODA;
+                cout << "\nВыбрано: Soda" << setw(27) << SODA << endl;
                 break;
             case 'C':
                 menuItem = CHICKEN_NUGGETS;
+                cout << "\nВыбрано: Chicken Nuggets" << setw(16) << CHICKEN_NUGGETS << endl;
                 break;
             case 'D':
                 menuItem = BREADSTICKS;
-                break;
-            case 'E':
+                cout << "\nВыбрано: Breadsticks" << setw(20) << BREADSTICKS << endl;
                 break;
             default:
-                cerr << "\nPlease pick a valid option\n";
+                cerr << "\nОшибка: Недопустимый код блюда\n";
+                continue; // Пропускаем недопустимые коды
         }
 
-        if (itemLetter == 'A' || itemLetter == 'B' || itemLetter == 'C' || itemLetter == 'D')
-            numItems++;
+        numItems++;
+        total += menuItem;
 
-        cout << "\nNumber of items: " << setprecision(0) << numItems << endl;
+        cout << "Количество блюд: " << setprecision(0) << numItems << endl;
+        cout << "Итоговая сумма: " << setprecision(2) << total << endl;
+    }
 
-        if (itemLetter == 'A' || itemLetter == 'B' || itemLetter == 'C' || itemLetter == 'D')
-            total += menuItem;
-
-        cout << "Total: " << setprecision(2) << total << endl;
-
-    } while (itemLetter != 'E');
-
-    cout << "\nThank you! Enjoy!" << endl;
+    cout << "\nСпасибо! Приятного аппетита!" << endl;
 
     return 0;
 }
 
 //*****************************************************************************************************
-/*
-
-=====================================================
-                Welcome to Pizza Palace
-=====================================================
------------------------------------------------------
-
-        A       Pizza                     15.50
-        B       Soda                       2.00
-        C       Chicken Nuggets            7.00
-        D       Breadsticks                9.75
-        E       Exit
-
-Please enter the next menu item Letter: A
-
-Number of items: 1
-Total: 15.50
------------------------------------------------------
-
-        A       Pizza                     15.50
-        B       Soda                       2.00
-        C       Chicken Nuggets            7.00
-        D       Breadsticks                9.75
-        E       Exit
-
-Please enter the next menu item Letter: S
-
-Please pick a valid option
-
-Number of items: 1
-Total: 15.50
------------------------------------------------------
-
-        A       Pizza                     15.50
-        B       Soda                       2.00
-        C       Chicken Nuggets            7.00
-        D       Breadsticks                9.75
-        E       Exit
-
-Please enter the next menu item Letter: d
-
-Please pick a valid option
-
-Number of items: 1
-Total: 15.50
------------------------------------------------------
-
-        A       Pizza                     15.50
-        B       Soda                       2.00
-        C       Chicken Nuggets            7.00
-        D       Breadsticks                9.75
-        E       Exit
-
-Please enter the next menu item Letter: D
-
-Number of items: 2
-Total: 25.25
------------------------------------------------------
-
-        A       Pizza                     15.50
-        B       Soda                       2.00
-        C       Chicken Nuggets            7.00
-        D       Breadsticks                9.75
-        E       Exit
-
-Please enter the next menu item Letter: C
-
-Number of items: 3
-Total: 32.25
------------------------------------------------------
-
-        A       Pizza                     15.50
-        B       Soda                       2.00
-        C       Chicken Nuggets            7.00
-        D       Breadsticks                9.75
-        E       Exit
-
-Please enter the next menu item Letter: E
-
-Number of items: 3
-Total: 32.25
-
-Thank you! Enjoy!
-
-*/
